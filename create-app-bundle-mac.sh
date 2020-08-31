451d77ccbb93555de7002ace3a62de2ab95a2b53
Gitea <gitea@fake.local>
2020-08-31 10:04:00 +0200

    Updated the macOS bundle creator script

--- create-app-bundle-mac.sh
+++ create-app-bundle-mac.sh
@@ -9,8 +9,15 @@ OSX_BUNDLE_ID="com.maxylabs.slt-qt"
 #----------------------
 pyinstaller ${FILE_APP} \
 --icon=${FILE_ICON} \
+--osx-bundle-identifier "${OSX_BUNDLE_ID}" \
 --windowed \
---osx-bundle-identifier "${OSX_BUNDLE_ID}"
+--onefile \
+--exclude-module='FixTk' \
+--exclude-module='tcl' \
+--exclude-module='tk' \
+--exclude-module='_tkinter' \
+--exclude-module='tkinter' \
+--exclude-module='Tkinter'
 
 # -- Copy the config folder
 cp -r ./config ./dist/slt-qt.app/Contents/MacOS
